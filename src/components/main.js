e37bbf5691897ecedf54faba8c4097b09f9011e9
Jecy Jehu <jecyjehu@outlook.com>
2023-01-30 14:10:01 +0100

    update

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,20 @@
+import frame from "../images/frame.png"
+
 function Main() {
     return(
-        <h1>h</h1>
+        <main className="bg-white py-8">
+            <form className=" flex flex-col px-5">
+                <div className="flex space-x-5 pb-7 w-full">
+                    <input className="border border-gray-400 rounded-lg w-full pl-2" type="text" placeholder="meme" />
+                    <input className="border border-gray-400 rounded-lg w-full pl-2" type="text" placeholder="generator" />
+                </div>                
+                <button className="flex border border-gray-400 rounded-lg px-3 w-full py-2 bg-gradient-to-r from-purple-600 via-purple-400 to-pink-500 text-center">
+                    Get a new Meme Image
+                    <img className="w-6" src={frame} alt="" />
+                </button>
+                <img src="" alt="" />
+            </form>
+        </main>
     )
 }
 
