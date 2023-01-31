69739a4f67e27ca9e92ca6efa10ce76656f9e43b
Jecy Jehu <jecyjehu@outlook.com>
2023-01-31 15:49:05 +0100

    update

--- src/components/main.js
+++ src/components/main.js
@@ -8,7 +8,7 @@ function Main() {
                     <input className="border border-gray-400 rounded-lg w-full pl-2" type="text" placeholder="meme" />
                     <input className="border border-gray-400 rounded-lg w-full pl-2" type="text" placeholder="generator" />
                 </div>                
-                <button className="flex border border-gray-400 rounded-lg px-3 w-full py-2 bg-gradient-to-r from-purple-600 via-purple-400 to-pink-500 text-center">
+                <button className="flex justify-center border border-gray-400 rounded-lg px-3 w-full py-2 bg-gradient-to-r from-purple-600 via-purple-400 to-pink-500">
                     Get a new Meme Image
                     <img className="w-6" src={frame} alt="" />
                 </button>
