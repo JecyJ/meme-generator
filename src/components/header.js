2221f13ee30ad78b2e113530284f18f0b7fbe138
Jecy Jehu <jecyjehu@outlook.com>
2023-02-06 16:37:16 +0100

    update

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function Header() {
     return(
         <header className="flex flex-col md:flex-row lg:flex-row items-center justify-between px-8 border bg-gradient-to-r from-purple-600 via-purple-400 to-pink-500 py-5 text-white">
         <div className="flex items-center text-3xl">
-            <img src={trollface} alt="" />
+            <img className="w-10" src={trollface} alt="" />
             <h1>Meme Generator</h1>
         </div>            
             <h2>React Course - Project 3</h2>
--- src/components/main.js
+++ src/components/main.js
@@ -1,19 +1,26 @@
 import frame from "../images/frame.png"
+import memesData from "./memesData";
 
 function Main() {
+    function getMemeImage() {
+        const memeArr = memesData.data.memes
+        const rand = Math.floor(Math.random() * memeArr.length)
+        const url = memeArr[rand].url
+        console.log(url)
+    }
     return(
         <main className="bg-white py-8">
-            <form className=" flex flex-col px-5">
+            <div className=" form flex flex-col px-5">
                 <div className="flex space-x-5 pb-7 w-full">
                     <input className="border border-gray-400 rounded-lg w-full pl-2" type="text" placeholder="meme" />
                     <input className="border border-gray-400 rounded-lg w-full pl-2" type="text" placeholder="generator" />
                 </div>                
-                <button className="flex justify-center border border-gray-400 rounded-lg px-3 w-full py-2 bg-gradient-to-r from-purple-600 via-purple-400 to-pink-500">
+                <button className="form-btn flex justify-center border border-gray-400 rounded-lg px-3 w-full py-2 bg-gradient-to-r from-purple-600 via-purple-400 to-pink-500" onClick={getMemeImage}>
                     Get a new Meme Image
                     <img className="w-6" src={frame} alt="" />
                 </button>
                 <img src="" alt="" />
-            </form>
+            </div>
         </main>
     )
 }
