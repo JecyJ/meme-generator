1370b8200bc098803fe3a056c6247cf661305e05
Jecy Jehu <jecyjehu@outlook.com>
2023-02-14 12:25:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Main />
         <Test />
         <Contact />
-        <Box />
+        <Box darkMode= {true} />
       </div>      
     </div>
   );
--- src/components/box.js
+++ src/components/box.js
@@ -1,16 +1,18 @@
 import setBoxes from "./boxes";
 import {useState} from "react"
 
-function Box() {
+function Box(props) {
     const [sqaures, setSquares] = useState(setBoxes)
+    const dark = props.darkMode
+
+    const styles = {
+        backgroundColor: dark ? '#222222' : '#cccccc'
+    }  
 
     const squareElements = sqaures.map(square => (
-        <div className="w-12 h-12 mt-5 border border-gray-500 rounded-lg mr-5 inline-block " key={setBoxes.id}></div>
+        <div style={styles} className="w-12 h-12 mt-5 border border-gray-500 rounded-lg mr-5 inline-block " key={setBoxes.id}></div>
     ))
 
-    
-
-   
 
     return (
         <main className="my-8">
