cf5ee4f1df6e6e328b2e94b88da0bcd60b94f73c
Jecy Jehu <jecyjehu@outlook.com>
2023-02-15 16:02:14 +0100

    update

--- src/components/box.js
+++ src/components/box.js
@@ -5,22 +5,12 @@ import Box1 from "./box1";
 function Box(props) {
     const [squares, setSquares] = useState(setBoxes)
     
-    function toggle(id) {        
+    function toggle(id) {
+        //This is a more concise way of doing it (imperative way). refer to the commented code below for the declarative way of doing it      
         return setSquares(prevSquares => {
-            const newSquares = []
-            for (let i = 0; i < prevSquares.length; i++) {
-                const currentSquares = prevSquares[i]
-                if(currentSquares.id === id) {
-                    const updatedSquare = {
-                        ... currentSquares, 
-                        on: !currentSquares.on
-                    }
-                    newSquares.push(updatedSquare)
-                } else {
-                    newSquares.push(currentSquares)
-                }
-            }
-            return newSquares
+           return prevSquares.map((square) => {
+                return square.id === id ? {...square, on: !square.on} : square
+           })
         })
     }
     
@@ -42,3 +32,24 @@ function Box(props) {
 }
 
 export default Box;
+
+// This is a more complex way of making the toggle to function properly
+
+// function toggle(id) {        
+//     return setSquares(prevSquares => {
+//         const newSquares = []
+//         for (let i = 0; i < prevSquares.length; i++) {
+//             const currentSquares = prevSquares[i]
+//             if(currentSquares.id === id) {
+//                 const updatedSquare = {
+//                     ... currentSquares, 
+//                     on: !currentSquares.on
+//                 }
+//                 newSquares.push(updatedSquare)
+//             } else {
+//                 newSquares.push(currentSquares)
+//             }
+//         }
+//         return newSquares
+//     })
+// }
