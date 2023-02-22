f7d20038e2edf704eac15cb9934299c0a3ae7d3b
Jecy Jehu <jecyjehu@outlook.com>
2023-02-22 21:01:06 +0100

    update

--- src/components/form.js
+++ src/components/form.js
@@ -1,20 +1,70 @@
 import { useState } from "react";
 
 function Form() {
-    const [firstName, setFirstName] = useState("")
+    const [formData, setFormData] = useState(
+        {
+            firstName: "",
+            middleName: "",
+            lastName: "",
+            email: "",
+            comments: ""
+        }
+    )
 
-    console.log(firstName)
+    console.log(formData)
 
     function handleChange(event){
-        return (setFirstName(event.target.value))
+        return setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
     }
+
+
+    
     return (
-        <div>
+        <div className="flex flex-col">
             <input
-                type = "Text"
+                className="rounded-lg px-2 w-64"
+                type = "text"
                 placeholder = "First Name"
                 onChange = {handleChange}
-            />           
+                name = "firstName"
+                value = {formData.firstName}
+            />
+               <input
+                className="mt-3 rounded-lg px-2 w-64"
+                type= "text"
+                placeholder="Middle Name"
+                onChange={handleChange}
+                name = "middleName"
+                value = {formData.middleName}
+            />
+            <input
+                className="mt-3 rounded-lg px-2 w-64"
+                type= "text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name = "lastName"
+                value = {formData.lastName}
+            />
+               <input
+                className="mt-3 rounded-lg px-2 w-64"
+                type= "email"
+                placeholder="Email"
+                onChange={handleChange}
+                name = "email"
+                value = {formData.email}
+            />
+            <textarea
+                className="mt-3 rounded-lg px-2 w-64"
+                placeholder="Comments"
+                onChange={handleChange}
+                name = "comments"
+                value={formData.comments}
+            />      
         </div>
     )
 }
